4529c9b116fb026e76647df90fd88268e36a9d1c
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-11-30 12:35:53 +0530

    Added:feature to add item in list

--- Main.java
+++ Main.java
@@ -5,7 +5,10 @@ public class Main {
     public static void main(String[] args) {
     ManualLinkedList  list= ManualLinkedList.List();
 
-
+list.add(70);
+list.add(30);
+list.add(56);
+list.display();
     }
 
 }
