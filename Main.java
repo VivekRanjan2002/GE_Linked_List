980b1b1268a902ea494ff25591f9ffd43b4d8422
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-11-30 12:39:08 +0530

    Refactored:add new test case to add item in list

--- Main.java
+++ Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
     ManualLinkedList  list= ManualLinkedList.List();
 
-list.add(70);
-list.add(30);
 list.add(56);
+list.add(30);
+list.add(70);
 list.display();
     }
 
