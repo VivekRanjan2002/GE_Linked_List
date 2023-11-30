229c2b0e81219bc102e68b32be516cb62f1cb219
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-11-30 14:33:13 +0530

    Added: unit test for search in list

--- ManualLinkedListTest.java
+++ ManualLinkedListTest.java
@@ -1,4 +1,20 @@
+package com.example.linkedlist;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ManualLinkedListTest {
-  
+   @Test
+   void Searchtest(){
+       ManualLinkedList list = new ManualLinkedList();
+       list.add(56);
+       list.add(30);
+       list.add(70);
+       assertEquals (true,list.search(70));
+       assertEquals (true,list.search(56));
+       assertEquals (true,list.search(30));
+       assertEquals (false,list.search(99));
+   }
+
 }
